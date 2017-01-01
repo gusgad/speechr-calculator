88869424769c1ef19841783cc033d087eeb74e67
Huseyn Gadirov <huseyngadirov@gmail.com>
2016-12-31 19:53:08 -0800

    Add JS to toggle sidebars

--- js/app.js
+++ js/app.js
@@ -1,15 +1,66 @@
-class SidebarMenu {
+class Sidebars {
     constructor(options) {
-        this.elem = options.elem;
+        this.elem = options;
+        this.wrapper = document.querySelector('.wrapper');
+        
+        // Settings sidebar elements
+        this.settingsPos = false;
+        this.settingsBtn = document.querySelector('.settings');
+        this.settingsMenu = document.querySelector('.settings_menu');
+        this.historyBtn = document.querySelector('.history');
+        
+        // History sidebar elements
+        this.hisorySidebar = document.querySelector('.history_sidebar');
+        this.historyBtn = document.querySelector('.history');
+        
+        this.addEventListeners();
+    }
+    addEventListeners() {
+        this.settingsBtn.addEventListener('click', (e) => {
+            e.stopPropagation();
+            this.openSettings();
+        });
+        this.wrapper.addEventListener('click', (e) => {
+            this.closeSettings();
+            this.closeHistory();
+        });
+        this.historyBtn.addEventListener('click', (e) => {
+            e.stopPropagation();
+            this.openHistory();
+        })
+    }
+    openSettings() {
+        this.settingsMenu.style.transform = 'translateX(-80%)';
+        this.settingsPos = true;
+    }
+    closeSettings() {
+        this.settingsMenu.style.transform = 'translateX(80%)';
+        this.settingsPos = false;
+    }
+    openHistory() {
+        this.hisorySidebar.style.transform = 'translateX(80%)';
+    }
+    closeHistory() {
+        this.hisorySidebar.style.transform = 'translateX(-80%)';
     }
 }
 
-let sidebarMenu = new SidebarMenu({
+new Sidebars();
+
+/*let sidebarMenu = new SidebarMenu({
     settingsBtn: document.querySelector('.settings'),
     settingsMenu: document.querySelector('.settings_menu')
 });
 
 let settingsBtn = document.querySelector('.settings');
 settingsBtn.addEventListener('click', function(e) {
-    alert('sup');
+    e.stopPropagation();
+    sidebarMenu.open();
 })
+
+let wrapper = document.querySelector('.wrapper');
+wrapper.addEventListener('click', function(e) {
+    if (sidebarMenu.position === true) {
+        sidebarMenu.close();
+    }
+})*/
