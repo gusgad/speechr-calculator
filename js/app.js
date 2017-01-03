5e8b12dc00efe783329e4df3192157e87e9c46da
Huseyn Gadirov <huseyngadirov@gmail.com>
2017-01-02 18:50:35 -0800

    Add waves lib and modify color menu

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+/* Waves library */
+Waves.attach('.record, .clear', ['waves-button']);
+Waves.attach('.history, .settings', ['waves-float']);
+Waves.init();
+
+
 class Sidebars {
     constructor(options) {
         this.elem = options;
@@ -12,6 +18,12 @@ class Sidebars {
         this.hisorySidebar = document.querySelector('.history_sidebar');
         this.historyBtn = document.querySelector('.history');
         
+        // Color menu
+        this.colorMenu = document.querySelector('.menu_color');
+        this.colorList = document.querySelector('.menu_color_list');
+        
+        // Font menu
+        this.fontMenu = document.querySelector('.menu_font');
         
         this.addEventListeners();
     }
@@ -29,6 +41,9 @@ class Sidebars {
             this.close(this.settingsMenu, '80');
             this.close(this.hisorySidebar, '-80');
         });
+        this.colorMenu.addEventListener('click', (e) => {
+            this.showMenu(this.colorList);
+        });
     }
     
     open(element, percent) {
@@ -44,6 +59,14 @@ class Sidebars {
         element.style.transform = `translateX(${percent}%)`;
         this.settingsBtn.style.pointerEvents = 'all';
         this.historyBtn.style.pointerEvents = 'all';
+    }
+    
+    showMenu(element) {
+        element.style.transform = 'translateY(0%)';
+    }
+    
+    hideMenu(element) {
+        element.style.transform = 'translateX(100%)';
     }
 }
 
@@ -61,34 +84,54 @@ class Recording {
         this.recognition = new SpeechRecognition();
         this.recognition.lang = 'en-IN';
         this.recognition.continuous = true;
+        this.transcript;
         //this.recognition.interimResults = true;
         
         this.statement = document.createElement('h5');
         this.result = document.createElement('h4');
         this.display = document.querySelector('.display');
         this.recordBtn = document.querySelector('.record');
+        this.clearBtn = document.querySelector('.clear');
         
         this.addEventListeners();
     }
     
     addEventListeners() {
         this.recognition.addEventListener('result', (e) => {
-            let transcript = Array.from(e.results)
+            this.transcript = Array.from(e.results)
                 .map(result => result[0])
-                .map(result => result.transcript);
-            
-            console.log(eval(transcript[0]));
+                .map(result => result.transcript)
             
             
-            this.statement.textContent = transcript;
+            // Checking multiplications for similar words
+            if (this.transcript[0].includes('multiply')) {
+                this.transcript[0] = this.transcript[0].replace('multiply', '*');
+            } else if (this.transcript[0].includes('x')) {
+                this.transcript[0] = this.transcript[0].replace('x', '*');
+            }
+            console.log(this.transcript);
+            
+            this.statement.textContent = this.transcript;
             this.display.appendChild(this.statement);
             
-            this.result.textContent = eval(transcript[0]);
+            this.result.textContent = eval(this.transcript[0]);
             this.display.appendChild(this.result);
         });
         this.recordBtn.addEventListener('click', (e) => {
-            this.recognition.start();
+            this.record();
         });
+        this.clearBtn.addEventListener('click', (e) => {
+            this.clear();
+        })
+    }
+    record() {
+        this.recognition.start();
+    }
+    clear() {
+        this.recognition.stop();
+        this.transcript = [];
+        this.statement.textContent = '';
+        this.result.textContent = '';
     }
 }
 
